6d348c0b72194c5145189f5c2c386d4f033d1f78
Si3ver <zhou_weilin@outlook.com>
2018-09-06 00:55:19 +0800

    拆分出actionTypes，防止隐晦的拼写错误！

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css';
 import store from './store';
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './store/actionTypes';
 // import axios from 'axios';
 
 class TodoList extends Component {
@@ -46,7 +47,7 @@ class TodoList extends Component {
 
     handleInputChange = (e) => {
         const action = {
-            type: 'change_input_value',
+            type: CHANGE_INPUT_VALUE,
             value: e.target.value
         };
         store.dispatch(action);
@@ -58,14 +59,14 @@ class TodoList extends Component {
 
     handleBtnClick = ()=>{
         const action = {
-            type: 'add_todo_item'
+            type: ADD_TODO_ITEM
         };
         store.dispatch(action);
     }
 
     handleItemDelete(idx){
         const action = {
-            type: 'delete_todo_item',
+            type: DELETE_TODO_ITEM,
             idx
         };
         store.dispatch(action);
