c835f47b5e40cff22e417df43da46fa1a0eb43a5
Si3ver <zhou_weilin@outlook.com>
2018-09-06 00:41:01 +0800

    使用redux实现todolist增删改功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,28 +2,73 @@ import React, { Component } from 'react';
 import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css';
 import store from './store';
+// import axios from 'axios';
 
 class TodoList extends Component {
+
     constructor(props) {
         super(props);
         this.state = store.getState();
-        console.log(this.state);
+        store.subscribe(this.handleStoreChange);
     }
+
     render(){
         return (
             <div style={{marginTop: '10px', marginLeft: '10px'}}>
                 <div>
-                    <Input value={this.state.inputValue} style={{width: '300px', marginRight: '10px'}} />
-                    <Button type="primary">submit</Button>
+                    <Input 
+                        value={this.state.inputValue} 
+                        style={{width: '300px', marginRight: '10px'}} 
+                        onChange={this.handleInputChange}
+                    />
+                    <Button type="primary" onClick={this.handleBtnClick}>submit</Button>
                 </div>
                 <List
                     style={{marginTop: '10px', width: '300px'}}
                     bordered
                     dataSource={this.state.list}
-                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                    renderItem={(item, idx)=>(
+                        <List.Item onClick={this.handleItemDelete.bind(this, idx)}>{item}</List.Item>
+                    )}
                 />
             </div>
         )
+    }
+    // componentDidMount() {
+    //     axios.get('/api/todolist')
+    //         .then((res)=>{
+    //             console.log(res.data);
+    //         })
+    //         .catch(()=>{
+    //             console.log('ajax /api/todolist 失败');
+    //         })
+    // }
+
+    handleInputChange = (e) => {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        };
+        store.dispatch(action);
+    }
+
+    handleStoreChange = ()=>{
+        this.setState(store.getState());
+    }
+
+    handleBtnClick = ()=>{
+        const action = {
+            type: 'add_todo_item'
+        };
+        store.dispatch(action);
+    }
+
+    handleItemDelete(idx){
+        const action = {
+            type: 'delete_todo_item',
+            idx
+        };
+        store.dispatch(action);
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,25 @@
 const defaultState = {
-    inputValue: '123',
-    list: [1, 2]
+    inputValue: '',
+    list: []
 };
 
+// reducer可以接收state, 但不能修改state, 所以下面做了深拷贝并返回新状态。
 export default (state = defaultState, action)=>{
+    if (action.type === 'change_input_value') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+    if (action.type === 'add_todo_item') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        return newState;
+    }
+    if (action.type === 'delete_todo_item') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.splice(action.idx, 1);
+        return newState;
+    }
     return state;
 };
